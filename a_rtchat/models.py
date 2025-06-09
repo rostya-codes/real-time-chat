66b61e96d1cd5e136c42d1103d52dbb9af551eb4
Rostya <rostyamudrik@proton.me>
2025-06-09 15:37:02 +0100

    private chat is done

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,10 +1,13 @@
+import shortuuid
 from django.conf import settings
 from django.db import models
 
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True)
+    group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
     users_online = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='online_in_groups', blank=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chat_groups', blank=True)
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
