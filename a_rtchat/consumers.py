1daada5f4317d8d9aaebfd381d15ebf53aa9a395
Rostya <rostyamudrik@proton.me>
2025-06-11 18:06:00 +0100

    Live activity monitoring added

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -2,10 +2,13 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 
 from a_rtchat.models import ChatGroup, GroupMessage
+
+User = get_user_model()
 
 
 class ChatroomConsumer(WebsocketConsumer):
@@ -58,6 +61,7 @@ class ChatroomConsumer(WebsocketConsumer):
         context = {
             'message': message,
             'user': self.user,
+            'chat_group': self.chatroom
         }
         html = render_to_string('a_rtchat/partials/chat_message_p.html', context=context)
         self.send(text_data=html)
@@ -73,9 +77,15 @@ class ChatroomConsumer(WebsocketConsumer):
 
     def online_count_handler(self, event):
         online_count = event['online_count']
+
+        chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_messages.all()[:30]
+        author_ids = set([message.author.id for message in chat_messages])
+        users = User.objects.filter(id__in=author_ids)
+
         context = {
             'online_count': online_count,
             'chat_group': self.chatroom,
+            'users': users,
         }
         html = render_to_string('a_rtchat/partials/online_count.html', context)
         self.send(text_data=html)
@@ -116,9 +126,24 @@ class OnlineStatusConsumer(WebsocketConsumer):
         )
 
     def online_status_handler(self, event):
-        online_users = self.group.users_online.exclude(id=self.user.id)
+        online_users = self.group.users_online  # .exclude(id=self.user.id)
+        public_chat_users = ChatGroup.objects.get(group_name='public-chat').users_online.exclude(id=self.user.id)
+
+        my_chats = self.user.chat_groups.all()
+        private_chats_with_users = [chat for chat in my_chats.filter(is_private=True) if chat.users_online.exclude(id=self.user.id)]
+        group_chats_with_users = [chat for chat in my_chats.filter(groupchat_name__isnull=False) if chat.users_online.exclude(id=self.user.id)]
+
+
+        if public_chat_users or private_chats_with_users or group_chats_with_users:
+            online_in_chats = True
+        else:
+            online_in_chats = False
+
         context = {
             'online_users': online_users,
+            'online_in_chats': online_in_chats,
+            'public_chat_users': public_chat_users,
+            'user': self.user,
         }
         html = render_to_string('a_rtchat/partials/online_status.html', context=context)
         self.send(text_data=html)
--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -11,7 +11,7 @@ from .forms import ChatMessageCreateForm, NewGroupForm, ChatRoomEditForm
 @login_required
 def chat_view(request, chatroom_name='public-chat'):
     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
-    chat_messages = chat_group.chat_messages.all()[:200]
+    chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatMessageCreateForm()
 
     other_user = None
